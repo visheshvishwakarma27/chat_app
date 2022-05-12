77154a92cd8b7cbdd5dd19ca2d2f6706ebf26ce5
Vishesh <visheshvishwakarma2700@gmail.com>
2022-05-12 13:28:14 +0530

    commit

--- project/settings.py
+++ project/settings.py
@@ -12,12 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from pickle import TRUE
-import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 
 
