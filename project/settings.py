e7bf098367ce250b7b4ef656cb2f26cb7d712f65
Vishesh <visheshvishwakarma2700@gmail.com>
2022-05-17 15:27:50 +0530

    new

--- project/settings.py
+++ project/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 from pickle import TRUE
 import environ
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
