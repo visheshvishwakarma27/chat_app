f23ca6f09d875203b8689e56e742276238f19cdb
Vishesh <visheshvishwakarma2700@gmail.com>
2022-05-17 15:08:42 +0530

    new

--- project/settings.py
+++ project/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': 5432
+        'PORT': 5432,
     }
 }
 print (DATABASES)
